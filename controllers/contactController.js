c74415f2be7b454e666de9ce0054c30d4bb6d9a7
Pavithra <pavir5587@gmail.com>
2026-05-30 14:26:52 +0530

    First commit

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -1,4 +1,4 @@
-const Contact = require("../models/Contact");
+const Contact = require("../models/contact");
 const nodemailer = require("nodemailer");
 // Add new contact
 exports.addContact = async (req, res) => {
--- controllers/courseController.js
+++ controllers/courseController.js
@@ -1,4 +1,4 @@
-const Course = require("../models/Course");
+const Course = require("../models/course");
 
 // Create
 exports.addCourse = async (req, res) => {
