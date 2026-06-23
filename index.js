496e1683b73a54e42948f500d3803e0d4f61658e
Pavithra <pavir5587@gmail.com>
2026-06-23 12:18:05 +0530

    cors changes

--- index.js
+++ index.js
@@ -37,8 +37,6 @@ const corsOptions = {
       "https://www.layartacademy.in",
     ],
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], // Allowed HTTP methods
-  allowedHeaders: ['Content-Type', 'Authorization'], // Headers your frontend sends
-  credentials: true // Crucial if you are sending cookies or session tokens
 };
 
 // Apply the CORS middleware BEFORE your routes
